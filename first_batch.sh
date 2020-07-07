6ab3c5c0b473aadb627ea937be37e948d7ce25c4
Ishiven <56818934+Ishiven@users.noreply.github.com>
2020-07-07 14:02:40 +0200

    Update first_batch.sh

--- first_batch.sh
+++ first_batch.sh
@@ -31,4 +31,4 @@
 
     #Seventh, replace the default AMI with our provided AMI
 
-    sed -i -e "s/ami-08935252a36e25f85/$(aws ec2 describe-images --owners self | grep "ImageId:*" | awk -F':' '{ print $2 }' | sed 's/\"//g' | sed 's/\,//g' | grep -v "ami-07273f1a8d9bb49c7")/g" taskstack/main.yml
+    sed -i -e "s/ami-08935252a36e25f85/$(aws ec2 describe-images --owners self | grep "ImageId:*" | awk -F':' '{ print $2 }' | sed 's/\"//g' | sed 's/\,//g' | grep -v "ami-07273f1a8d9bb49c7")/g" main.yml
