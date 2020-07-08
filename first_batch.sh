be7e62e28f80b6c610416b7158c328cced8a37bd
Ishiven <56818934+Ishiven@users.noreply.github.com>
2020-07-08 13:54:51 +0200

    Update first_batch.sh

--- first_batch.sh
+++ first_batch.sh
@@ -2,7 +2,7 @@
 
     #Deploy an instance in selected region with custom script included in the local repo
 
-    aws ec2 run-instances --image-id ami-0c3e74fa87d2a4227 --count 1 --key-name stack_key_pair --instance-type t2.micro  --security-group-ids sg-0b3df30e75c5ec04a --subnet-id subnet-0014b42dc6eb87008 --user-data file://config.sh --iam-instance-profile Name=adminrole
+    aws ec2 run-instances --image-id ami-0c3e74fa87d2a4227 --count 1 --key-name stack_key_pair --region eu-west-1 --instance-type t2.micro  --security-group-ids sg-0b3df30e75c5ec04a --subnet-id subnet-0014b42dc6eb87008 --user-data file://config.sh --iam-instance-profile Name=adminrole
     
     #Replace the default AMI with our newly generated AMI
 
