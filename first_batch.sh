88e96a682e4fd3ceec3f0b56388a2ee3a5375173
Ishiven <56818934+Ishiven@users.noreply.github.com>
2020-07-08 14:10:00 +0200

    Update first_batch.sh

--- first_batch.sh
+++ first_batch.sh
@@ -8,7 +8,6 @@
     
     # sleep 120s
     
-    aws ec2 describe-images --region eu-west-1 --owners self | grep "ImageId:*" | awk -F':' '{ print $2 }' | sed 's/\"//g' | sed 's/\,//g' | sed 's/^[[:space:]]*//g' 
-    #|  grep -v "ami-07273f1a8d9bb49c7"
+    aws ec2 describe-images --region eu-west-1 --owners self | grep "ImageId:*" | awk -F':' '{ print $2 }' | sed 's/\"//g' | sed 's/\,//g' | sed 's/^[[:space:]]*//g' | grep -v "ami-07273f1a8d9bb49c7"
     
     #sudo sed -e "s/ami-08935252a36e25f85/$(aws ec2 describe-images --region eu-west-1 --owners self | grep "ImageId:*" | awk -F':' '{ print $2 }' | sed 's/\"//g' | sed 's/\,//g' | sed 's/^[[:space:]]*//g' |  grep -v "ami-07273f1a8d9bb49c7")/g" main.yml
